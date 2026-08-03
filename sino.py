aa2fc89264aa154720a409c4e67f87a646bfc66a
nigelhayes11 <manus11@linuxmail.org>
2026-08-03 10:28:44 +0300

    Update sino.py

--- sino.py
+++ sino.py
@@ -49,7 +49,7 @@ for ch in channels:
     m3u_lines.append(url)
 
 # Dosyaya yaz
-with open("zeus.m3u", "w", encoding="utf-8") as f:
+with open("salam.m3u", "w", encoding="utf-8") as f:
     f.write("\n".join(m3u_lines))
 
-print("\n✅ zeus.m3u başarıyla oluşturuldu!")
+print("\n✅ salam.m3u başarıyla oluşturuldu!")
